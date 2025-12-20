2b6a0092e5521124707022365fe4e310b8f929d6
Teerapong <teerapong.ibenz@gmail.com>
2025-12-20 18:15:10 +0700

    create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Treebuger JS loaded");
+
+const subtitle = document.getElementById("subtitle");
+subtitle.textContent = "Web & Manual Software Testing";
+
+const btn = document.getElementById("contactBtn");
+btn.addEventListener("click", () => {
+  alert("Email: Teerapong.ibenz@gmail.com");
+});
